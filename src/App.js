f7a4116f08a54da458099c9e683c1ef8687924b2
Suresh Reddy <113485687+Suriashwani@users.noreply.github.com>
2023-03-01 18:55:37 +0530

    feature/animation added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
 import './App.css';
-import {Routes,Route} from 'react-router-dom';
+import {Routes,Route,useLocation} from 'react-router-dom';
 import Home from './containers/home';
 import About from './containers/about';
 import Resume from './containers/resume';
 import Portfolio from './containers/portfolio';
 import Skills from './containers/skills';
 import Contact from './containers/contact';
+import Navbar from './components/navBar';
+import Particles from 'react-tsparticles';
+import {loadFull} from 'tsparticles';
+// import my_obj from "./utils/particle.json"
+//const particle = require("../utils/particle.json");
+import particle from "./utils/particle";
+import {useCallback} from 'react';
+
 
 function App() {
+
+        const location = useLocation();
+        console.log(location);
+
+        const showParticleJsInHomePage = location.pathname === "/";
+
+    const particlesInit = useCallback(async (main)=>{
+      console.log(main);
+      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+      // starting from v2 you can add only the features you need reducing the bundle size
+      await loadFull(main);
+    }, []);
+    const particlesLoaded =useCallback( async container => {
+      await console.log(container);
+  }, []);
+  
+
+    
   return (
     <div className="App">
       {/* particles js */}
-
+       
+      {
+        showParticleJsInHomePage && 
+        <Particles id="tsparticles" init={particlesInit}  options={particle} loaded={particlesLoaded} />
+      }
       {/* navbar */}
-
+        <Navbar />
       {/* main page content */}
 
       {/* routings */}
--- src/components/navBar/index.jsx
+++ src/components/navBar/index.jsx
@@ -1,10 +1,72 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+import {FaBars,FaReact} from 'react-icons/fa';
+import {HiX} from 'react-icons/hi';
+import './styles.scss';
 
-const NavBar = ()=>{
+
+const data = [ {
+               label:'HOME',
+               to:'/'
+               },
+               {
+                label:'ABOUT ME',
+                to:'/about'
+                },
+                {
+                    label:'SKILLS',
+                    to:'/skills'
+                },
+                {
+                    label:'RESUME',
+                    to:'/resume'
+                },
+                {
+                    label:'PORTFOLIO',
+                    to:'/portfolio'
+                },
+                {
+                    label:'CONTACT',
+                    to:'/contact'
+                },
+            ];
+               
+
+
+const Navbar = ()=>{
+
+    const [toggleIcon, setToggleIcon] = useState(false);
+
+    const handleToggleIcon = ()=>{
+        setToggleIcon(!toggleIcon);
+    }
     return (
         <>
-        
+            <div className="navbar">
+                <div className="navbar__container">
+                    <Link to="/" className="navbar__logo">
+                    <FaReact size={40} />
+                    </Link>
+                </div>
+                <ul className={`navbar__menu ${toggleIcon? 'active': ''}`}>
+                    {
+                        data.map( (element, index)=> (
+                            <li key={index} className="navbar__menu__item">
+                                <Link className="navbar__menu__item__links" to={element.to}>
+                                    {element.label}
+                                </Link>
+                            </li>
+                        ))
+                    }
+                </ul>
+                
+                <div className="navbar__menuicon" onClick={handleToggleIcon}>
+                    {
+                        toggleIcon? <HiX size={30} /> : <FaBars size={30} /> 
+                    }
+                </div> 
+            </div>
         </>
     );
 }
-export default NavBar;
+export default Navbar;
